c310a556ca010670b71b6e94825e348b9f9ecf36
Matthew Wire <mjw@mjwconsult.co.uk>
2017-07-24 22:53:09 +0100

    Use strpos for performance

--- CRM/Customexport/Versandtool.php
+++ CRM/Customexport/Versandtool.php
@@ -311,7 +311,7 @@ WHERE contact_id BETWEEN {$startContactId} AND {$endContactId}
    */
   private function filterExternalContactIds(&$contacts) {
     foreach ($contacts as $id => $data) {
-      if (substr($data['external_identifier'], 0, 4) != 'IMB-') {
+      if (strpos($data['external_identifier'], 'IMB-', 0) !== TRUE) {
         $contacts['id']['external_identifier'] = NULL;
       }
     }
