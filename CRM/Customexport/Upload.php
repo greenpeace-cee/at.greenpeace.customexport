9fa84e1b54fe43d1c2b7bbdecf077450c06cd5cd
Matthew Wire <mjw@mjwconsult.co.uk>
2017-07-24 21:21:30 +0100

    Use phpseclib instead of curl for sftp uploads

--- CRM/Customexport/Upload.php
+++ CRM/Customexport/Upload.php
@@ -56,7 +56,25 @@ class CRM_Customexport_Upload {
         $host = $host . '/';
       }
     }
-    $this->url = $host;
+    else {
+      // If fullURI we need to break it up
+      // FIXME: This could do with some error checking!
+      $index = strpos($host, '://') + 3;
+      $protocol = substr($host, 0, $index);
+      $host = substr($host, $index);
+      $index = strpos($host, '@');
+      $userArr = explode(':', substr($host, 0, $index));
+      $this->user = $userArr[0];
+      $this->password = isset($userArr[1]) ? $userArr[1] : NULL;
+      $host = substr($host, $index);
+      $index = strpos($host, '/');
+      if ($index !== FALSE) {
+        $hostname = substr($host, 1, $index -1);
+        $path2 = substr($host, $index);
+        $path = $path2 . $path;
+      }
+    }
+    $this->url = $hostname;
     $this->remotePath = $path;
   }
 
@@ -65,31 +83,52 @@ class CRM_Customexport_Upload {
    * @return int: This is the error code from curl, 0 is success!  Anything else is failure
    */
   function upload() {
-    $ch = curl_init();
-    $localfile = $this->localFile;
-    $fp = fopen($localfile, 'r');
-    curl_setopt($ch, CURLOPT_URL, $this->url.$this->remotePath);
-    curl_setopt($ch, CURLOPT_UPLOAD, 1);
-    switch ($this->method) {
-      case 'sftp':
-      default:
-      curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_SFTP);
-    }
-    curl_setopt($ch, CURLOPT_INFILE, $fp);
-    curl_setopt($ch, CURLOPT_INFILESIZE, filesize($localfile));
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-    curl_exec ($ch);
-    $error_no = curl_errno($ch);
-    if ($error_no == 0) {
-      $this->error = 'File uploaded succesfully.';
-    } else {
-      $this->error = curl_error($ch);
+    require_once('Net/SFTP.php');
+
+    define('NET_SFTP_LOGGING', NET_SFTP_LOG_SIMPLE);
+
+    $sftp = new Net_SFTP($this->url);
+    if (!$sftp->login($this->user, $this->password)) {
+      $this->error = 'Login Failed';
+      return 1;
     }
 
-    curl_close ($ch);
-
-    return $error_no;
+    // puts an x-byte file named filename.remote on the SFTP server,
+    // where x is the size of filename.local
+    $result = $sftp->put($this->remotePath, $this->localFile, NET_SFTP_LOCAL_FILE);
+    $this->error = $sftp->getSFTPLog();
+    if ($result) {
+      return 0;
+    }
+    return 1;
   }
+
+  /*  function upload() {
+      $ch = curl_init();
+      $localfile = $this->localFile;
+      $fp = fopen($localfile, 'r');
+      curl_setopt($ch, CURLOPT_URL, $this->url.$this->remotePath);
+      curl_setopt($ch, CURLOPT_UPLOAD, 1);
+      switch ($this->method) {
+        case 'sftp':
+        default:
+        curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_SFTP);
+      }
+      curl_setopt($ch, CURLOPT_INFILE, $fp);
+      curl_setopt($ch, CURLOPT_INFILESIZE, filesize($localfile));
+      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+      curl_exec ($ch);
+      $error_no = curl_errno($ch);
+      if ($error_no == 0) {
+        $this->error = 'File uploaded succesfully.';
+      } else {
+        $this->error = curl_error($ch);
+      }
+
+      curl_close ($ch);
+
+      return $error_no;
+    }*/
 
   function getErrorMessage() {
     return $this->error;
