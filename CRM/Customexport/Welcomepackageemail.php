92a521728ead46c09dcf38ffbc31c4a1e419076d
Matthew Wire <mjw@mjwconsult.co.uk>
2017-08-03 12:27:34 +0100

    Update welcomepackageemail query

--- CRM/Customexport/Welcomepackageemail.php
+++ CRM/Customexport/Welcomepackageemail.php
@@ -85,8 +85,8 @@ CREATE TABLE IF NOT EXISTS temp_welcome AS
       , 0 					AS keep_contact
 	FROM civicrm_contact c
 		LEFT JOIN civicrm_email email ON email.contact_id=c.id  AND is_primary=1
-	WHERE c.is_deceased=0 AND c.is_deleted=0
-    AND c.do_not_email=0 AND c.is_opt_out=0 AND email.email IS NOT NULL
+	WHERE c.is_deceased=0 AND c.is_deleted=0  
+		AND c.do_not_email=0 AND c.is_opt_out=0 AND email.email IS NOT NULL
     );
     ";
 
@@ -95,44 +95,35 @@ CREATE TABLE IF NOT EXISTS temp_welcome AS
 /*#Table with all contacts, which should not receive any welcome mail*/
     $sql[]="DROP TABLE IF EXISTS temp_welcome_delete;";
     $sql[]="
-CREATE TABLE IF NOT EXISTS temp_welcome_delete AS
+CREATE TABLE IF NOT EXISTS temp_welcome_delete AS 
 	(
-  SELECT DISTINCT contact_id
-    FROM (
-    #OUT: inaktiv, VIP, Firma, Schule
-      SELECT DISTINCT et.entity_id AS contact_id
-			FROM civicrm_entity_tag et
-			LEFT JOIN civicrm_tag ct ON et.tag_id=ct.id
-			WHERE et.entity_table = 'civicrm_contact' AND ct.name IN ('inaktiv','VIP')
-
+	SELECT DISTINCT contact_id
+    FROM (	
+			#OUT: inaktiv, VIP, Firma, Schule 
+			SELECT DISTINCT et.entity_id AS contact_id
+			FROM civicrm_entity_tag et 		
+			LEFT JOIN civicrm_tag ct ON et.tag_id=ct.id 
+			WHERE et.entity_table = 'civicrm_contact' AND ct.name IN ('inaktiv','VIP') 
+			
 			UNION ALL
-
+            
 			#OUT: Erblasser
 			SELECT DISTINCT group_c.contact_id
-			FROM civicrm_group_contact group_c
+			FROM civicrm_group_contact group_c 
 			LEFT JOIN civicrm_group g ON g.id=group_c.group_id
 			WHERE g.title='Erblasser'
-
+			
 			UNION ALL
-
+			
             #OUT: Major Donors
 			SELECT cb.contact_id
-			FROM civicrm_contribution AS cb
+			FROM civicrm_contribution AS cb 
 			WHERE receive_date >= NOW() - INTERVAL 1 YEAR
 			GROUP BY cb.contact_id
 			HAVING SUM(cb.total_amount)>=1000
-
-            UNION ALL
-
-            #OUT: Emailhistory in last 6 Months
-            SELECT ac.contact_id
-			FROM civicrm_activity AS activity
-			LEFT JOIN civicrm_activity_contact AS ac ON ac.activity_id=activity.id
-			LEFT JOIN civicrm_campaign AS campaign ON campaign.id=activity.campaign_id
-			WHERE campaign.external_identifier='{$this->campaignExternalIdentifier}' AND activity_date_time >= NOW()-INTERVAL 6 MONTH
-
+                        
             #OUT: Hard/Softbounces - not in Civi, yet! when implemented, then this has to be updated
-
+            
 		) AS delete_multiple_contacts
     );
     ";
@@ -142,7 +133,7 @@ CREATE TABLE IF NOT EXISTS temp_welcome_delete AS
 /*#Delete all contacts from the welcome-list which were collected in the delete-table*/
     $sql[]="
 DELETE
-FROM temp_welcome
+FROM temp_welcome 
 WHERE keep_contact=0 AND contact_id IN (SELECT contact_id FROM temp_welcome_delete)
 ;
     ";
@@ -155,13 +146,13 @@ WHERE keep_contact=0 AND contact_id IN (SELECT contact_id FROM temp_welcome_dele
 #IN: Membership Current/ Paused, Sepa RCUR / FRST, join date in last 6 months*/
     $sql[]="DROP TABLE IF EXISTS temp_welcome_keep;";
     $sql[]="
-CREATE TABLE IF NOT EXISTS temp_welcome_keep AS
+CREATE TABLE IF NOT EXISTS temp_welcome_keep AS 
 	(
-  SELECT DISTINCT contact_id
+    SELECT DISTINCT contact_id
 	FROM civicrm_membership AS m
 	LEFT JOIN civicrm_membership_status AS ms ON m.status_id=ms.id
 	LEFT JOIN civicrm_value_membership_payment AS mp ON mp.entity_id=m.id
-	LEFT JOIN civicrm_option_value v ON v.value=mp.payment_instrument AND v.option_group_id=10
+	LEFT JOIN civicrm_option_value v ON v.value=mp.payment_instrument AND v.option_group_id=(SELECT id FROM civicrm_option_group WHERE name ='payment_instrument')
 	WHERE ms.label IN ('Current','Paused') AND v.name IN ('RCUR','FRST')  AND join_date >= (NOW() - INTERVAL 6 MONTH) AND join_date < (NOW() - INTERVAL 2 DAY)
     );
     ";
@@ -169,7 +160,7 @@ CREATE TABLE IF NOT EXISTS temp_welcome_keep AS
 
 /*#Mark the contacts, which should definetly be on the welcome list*/
     $sql[]="
-UPDATE temp_welcome
+UPDATE temp_welcome 
 SET  keep_contact=1
 WHERE contact_id IN (SELECT contact_id FROM temp_welcome_keep);
     ";
